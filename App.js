74df3860ec5cbdce4ba404f803b1b7775a31b5a3
yash seth <yashseth07@gmail.com>
2024-10-03 23:35:21 +0530

    updated packages

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from 'react';
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+// import 'core-js/stable';
+// import 'regenerator-runtime/runtime';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,6 @@ module.exports = {
   presets: ['babel-preset-expo'],
   plugins: [
     ['@babel/plugin-transform-runtime', { regenerator: true }],
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-      },
-    ],
-    'react-native-reanimated/plugin', // Add this for react-native-reanimated
+    'react-native-reanimated/plugin', // Ensure this is the last plugin
   ],
 };
